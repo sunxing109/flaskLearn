5d467d3706813f393c818c6225bcedf8329806dd
sunxingba <sunxingba@e-u.cn>
2019-02-14 11:53:34 +0800

    简单的表单校验的web demo

--- flask_Learn/views.py
+++ flask_Learn/views.py
@@ -31,6 +31,10 @@ def hello():
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect('/hello')
     return render_template('login.html',
         title = 'Sign In',
-        form = form)
+        form = form,
+        providers=app.config['OPENID_PROVIDERS'])
